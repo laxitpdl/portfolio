916d8a945ed9bd35d86f50a7f7fcf7bbd53d84ef
laxitpdl <poudellakshit042061@gmail.com>
2025-07-06 16:21:11 -0500

    new

--- portfolio.py
+++ portfolio.py
@@ -7,13 +7,15 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1: 
-    st.image("npl.png")
+    st.image("images/npl.png")
 
 with col2:
     st.title("Lakshit Poudel")
     comment = """ Hi! I’m Lakshit Poudel, an aspiring programmer originally from Nepal. Currently based in the U.S. I am a Self-taught and hyper-focused, I blend engineering logic with AI tools to build real-world apps — from Python automation to Streamlit web apps, from machine learning models to full-stack dev.
 Fluent in both code and calculation, I aim to solve meaningful problems and push my limits — whether in tech or in life."""
-    st.info(comment)
+    st.write(comment)
               
 
+comment2 = """ Below you can see my apps and fell free to share your experience!!"""
 
+st.info(comment2) 
