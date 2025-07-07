d2fe87ebacde0b6e064a9a557ab98f98c8016a14
laxitpdl <poudellakshit042061@gmail.com>
2025-07-06 21:00:49 -0500

    added responsive email and i am confused as hell

---
+++ send_email.py
@@ -0,0 +1,20 @@
+import smtplib, ssl
+
+def send_email(message):
+
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "poudellakshit0@gmail.com"
+    password = "jeopmqeeijvdbigw"
+
+    receiver = "poudellakshit0@gmail.com"
+    context = ssl.create_default_context()
+
+    
+
+    with smtplib.SMTP_SSL(host, port, context=context) as  server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message )
+
+
