8567f49becb3c2523ca1dd7f9268a2fb9e484b1f
laxitpdl <poudellakshit042061@gmail.com>
2025-07-06 20:09:06 -0500

    send email

--- pages/Contact_us.py
+++ pages/Contact_us.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.header("Contact us")
+
+with st.form(key = "email_forms" ):
+    user_email= st.text_input("Your mail...")
+    message = st.text_area("Your message...")
+
+    button = st.form_submit_button()
